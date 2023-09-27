d4e97992a25fe412549460d6bd4a12680e08ea5f
Lakshman Karthik Ramkumar <latchukarthick98@gmail.com>
2023-09-26 18:12:16 -0700

    Initialize Gin and implemented Environment helper

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+// Helper function to retrieve env variable
+func getEnvVar(key string) string {
+
+	// Load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
 
 func main() {
-	fmt.Println("Hello, Go!")
+	// Initialize Gin
+	r := gin.Default()
+
+	// Get PORT number from .env file
+	port := getEnvVar("PORT")
+
+	// routes.InitRouter(r)
+	fmt.Printf("Running on Port %s \n", port)
+	// Initialze Server on Port 3001
+	r.Run(":" + port)
 }
