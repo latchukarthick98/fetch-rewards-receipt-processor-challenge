f2d72b034467d77e9ef9c77c7e711aae2317c1f1
Lakshman Karthik Ramkumar <latchukarthick98@gmail.com>
2023-09-26 19:09:40 -0700

    Implement datastore for points for each receipt

---
+++ datastore/datastore.go
@@ -0,0 +1,8 @@
+/**
+*	Created by Lakshman Karthik Ramkumar (latchukarthick98) on 11/03/2022
+ */
+
+package datastore
+
+// Maps the points assigned to each receipt where the key is the ID generated when the receipt is posted.
+var Points map[string]int = make(map[string]int)
--- main.go
+++ main.go
@@ -1,3 +1,7 @@
+/**
+*	Created by Lakshman Karthik Ramkumar (latchukarthick98) on 09/26/2023
+ */
+
 package main
 
 import (
