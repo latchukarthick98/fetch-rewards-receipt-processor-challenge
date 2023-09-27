f77c0ed4c56bbf090e7d07ec3aa7fbff61642d6d
Lakshman Karthik Ramkumar <latchukarthick98@gmail.com>
2023-09-26 19:11:04 -0700

    Implemented CORS middleware

---
+++ middlewares/cors.go
@@ -0,0 +1,23 @@
+/**
+*	Created by Lakshman Karthik Ramkumar (latchukarthick98) on 09/26/2023
+ */
+
+package middlewares
+
+import "github.com/gin-gonic/gin"
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if ctx.Request.Method == "OPTIONS" {
+			ctx.AbortWithStatus(204)
+			return
+		}
+
+		ctx.Next()
+	}
+}
